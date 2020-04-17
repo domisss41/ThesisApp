9e7a49c89d6d0a0dfc9a615e7a72690a3d643457
Adam <adamkuzanski@gmail.com>
2020-04-17 21:54:33 +0200

    dziala

--- App/app/src/main/java/com/example/thesisapp/User.kt
+++ App/app/src/main/java/com/example/thesisapp/User.kt
@@ -2,7 +2,7 @@ package com.example.thesisapp
 
 class User (
     val id: String = "",
-    val login: String ="",
+    val login: String = "",
     val email: String = "",
     val password: String = "",
     val birth: String = ""
