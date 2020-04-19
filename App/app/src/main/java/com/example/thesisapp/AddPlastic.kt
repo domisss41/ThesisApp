3f90813c0c3d85f18251385c516459e545cb6c57
Adam <adamkuzanski@gmail.com>
2020-04-19 20:01:14 +0200

    Plasticzek

--- App/app/src/main/java/com/example/thesisapp/AddPlastic.kt
+++ App/app/src/main/java/com/example/thesisapp/AddPlastic.kt
@@ -1,8 +1,6 @@
 package com.example.thesisapp
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
