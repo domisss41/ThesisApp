c302935a7a3181039c44f544e80810b4ad827c17
Adam <adamkuzanski@gmail.com>
2020-04-21 14:08:02 +0200

    Expandable List działa

---
+++ App/app/src/main/java/com/example/thesisapp/PlasticExpandableListView.kt
@@ -0,0 +1,12 @@
+package com.example.thesisapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlasticExpandableListView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plastic_expandable_list_view)
+    }
+}
