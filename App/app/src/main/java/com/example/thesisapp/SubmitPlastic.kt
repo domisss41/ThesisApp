adb82a50348ab6d9e99821d916343733a02bf5d4
Adam <adamkuzanski@gmail.com>
2020-06-12 00:03:42 +0200

    prawiegit

---
+++ App/app/src/main/java/com/example/thesisapp/SubmitPlastic.kt
@@ -0,0 +1,12 @@
+package com.example.thesisapp.expandableListAddPlastic
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SubmitPlastic : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_submit_plastic)
+    }
+}
